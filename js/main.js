0dced5e0b90625bac549d3fa80d4ed4848443a34
itimetravelwell <charles.jackson@georgebrown.ca>
2015-09-25 14:19:34 -0400

    Step 1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// when you "click" signin, "fadein" "modal"
+$('.signin')//if this than that
+	.on('click', function() {
+		$('.modal').fadeIn("slow");
+});//This works now dont touch or combine until you know it works.
